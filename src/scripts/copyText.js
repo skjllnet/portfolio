5a33ba65d85590ee22cdc6300c9c572e0682761a
Vassil Kurktchiev <vassil.kurk@gmail.com>
2022-08-31 15:27:17 -0400

    pushing for live

--- src/scripts/copyText.js
+++ src/scripts/copyText.js
@@ -1,14 +1,6 @@
-// function copyText() {
-//   let text = document.getElementById("myEmail");
-
-//   text.select();
-//   text.setSelectionRange(0, 99999);
-
-//   navigator.clipboard.writeText(text.value);
-// }
-
 const btn_copy = document.getElementById("copy");
 const txt_data = document.getElementById("data");
+
 copy.onclick = () => {
   const value = txt_data.value;
   navigator.clipboard.writeText(value);
