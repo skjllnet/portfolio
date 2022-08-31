77cca822b18ff7b3df9cfd86604dc80489aca0e7
Vassil Kurktchiev <vassil.kurk@gmail.com>
2022-08-31 11:18:15 -0400

    added clipboard resume and current year functionalities

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 import copyText from "./scripts/copyText";
+import theYear from "./scripts/theYear";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
 copyText();
+theYear();
